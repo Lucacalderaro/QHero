6a6c2cfa9d6ca3dea88f32c5cfe354609f97ad09
sydanjainen <andrea.chinello94#gmail.com>
2019-11-05 14:28:35 +0100

    Update sketch.js

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -2,13 +2,22 @@ var rows = 4;
 var cols = 20;
 var w;
 var h;
+var mid;
+var framePress; 
 var grid = new Array(rows);
+
+
 function setup() {
+  // setting up env
   createCanvas(400, 700);
+  frameRate(100);
+
+  //setting var
   w = width / rows;
   h = height / cols;
-frameRate(30);
+  mid = floor(cols/3);
 
+  // init grid
   for (var i = 0; i < rows; i++) {
     grid[i] = new Array(cols);
   }
@@ -18,15 +27,68 @@ frameRate(30);
       grid[i][j] = new Cell(i, j);
     }
   }
-  grid[0][0].nota = true;
 }
 
 function draw() {
+  rendering();
+  physical();
+}
+
+
+//visualizzazione tasti pigiati
+function keyTyped() {
+  if (key   === 'a' && framePress) {
+    grid[0][mid].nota = true;
+    framePress = false;
+    sendInfo('a');
+  }else if (key   === 's' && framePress) {
+    grid[1][mid].nota = true;
+    framePress = false;
+    sendInfo('s');
+  }else if (key   === 'd' && framePress) {
+    grid[2][mid].nota = true;
+    framePress = false;
+    sendInfo('d');
+  }else if (key   === 'f' && framePress) {
+    grid[3][mid].nota = true;
+    framePress = false;
+    sendInfo('f');
+  }
+}
+
+function physical(){
   background(220);
+  //setting framePress to true
+  framePress = true;
+
+  //visualizzazione griglia
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
       grid[i][j].show();
     }
   }
+
+  //aggiornamento griglia
+  for (var j = cols-1; j > 1; j--) {
+    for (var i = 0; i < rows; i++) {
+      if(j==cols-1){
+        grid[i][j].nota = false
+      }
+      if(j-1){
+        if(grid[i][j-1].nota){
+          grid[i][j].nota = true;
+          grid[i][j-1].nota = false;
+        }
+      }
+    }
+  }
+
+}
+
+function rendering(){
+  console.log("ciao");
+}
+
+function sendInfo(a){
 
 }
