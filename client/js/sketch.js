6ffb8b11bdda3f745574f3372d16f81345963324
sydanjainen <andrea.chinello94#gmail.com>
2019-11-05 12:57:33 +0100

    client update grid

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,7 +1,32 @@
+var rows = 4;
+var cols = 20;
+var w;
+var h;
+var grid = new Array(rows);
 function setup() {
-  // put setup code here
+  createCanvas(400, 700);
+  w = width / rows;
+  h = height / cols;
+frameRate(30);
+
+  for (var i = 0; i < rows; i++) {
+    grid[i] = new Array(cols);
+  }
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      grid[i][j] = new Cell(i, j);
+    }
+  }
+  grid[0][0].nota = true;
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      grid[i][j].show();
+    }
+  }
+
 }
