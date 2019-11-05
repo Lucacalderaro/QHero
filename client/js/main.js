470804139b50cc224b4045d25d7d5f7cf9482534
Federico Berra <fede.berra@hotmail.it>
2019-11-05 17:00:49 +0100

    Added start menu

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
   console.log("ready!");
+  $('#myModal').modal({
+    show: true
+  })
   setTimeout(function () {
     $("#sketch-holder").find("canvas").css("width", "100%");
     $("#sketch-holder").css("margin-top", "200px");
